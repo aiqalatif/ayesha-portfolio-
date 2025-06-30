4c2331fba29f42a352e9dbe84cc4f6909f8580a1
aiqalatif <ayeshalatif55566@gmail.com>
2025-06-30 01:50:12 -0700

    fix: add base href for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -57,14 +57,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "14e82711a42ebd7d33e4e9271b946d13
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "4e989c43de5f28a23d235ada4b583f31",
+"flutter_bootstrap.js": "844a33ea051ccf7b66741ad1861de672",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1058a4a0a0b5b104ea6244cddc6d9778",
-"/": "1058a4a0a0b5b104ea6244cddc6d9778",
-"main.dart.js": "f1f4938446f8b5268d223ace2bedf996",
+"index.html": "91f2f0a32c21eee266bfff824c2a1d2a",
+"/": "91f2f0a32c21eee266bfff824c2a1d2a",
+"main.dart.js": "e9baf598188f4e6e25f993afa11b6a83",
 "manifest.json": "30a6ea85f38555c77870a57aeabe8211",
 "version.json": "c420e205d8eac4ccea99913b4a50bfa7"};
 // The application shell files that are downloaded before a service worker can
